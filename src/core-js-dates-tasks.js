70ae2035d9024ff41def683dd3a8cb0ce58c49e8
Tatsiana Fedaruk <tatyanafedoruk@icloud.com>
2024-03-25 20:29:30 +0100

    fix: work schedule

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -299,7 +299,12 @@ function getWorkSchedule(period, countWorkDays, countOffDays) {
   let nextDay = 1;
   while (currentDate <= end) {
     if (countWorkDays >= workCount) {
-      schedule.push(currentDate.toLocaleDateString().split('.').join('-'));
+      const day = currentDate.getDate();
+      const month = currentDate.getMonth() + 1;
+      const year = currentDate.getFullYear();
+      schedule.push(
+        `${day.toString().padStart(2, '0')}-${month.toString().padStart(2, '0')}-${year}`
+      );
       workCount += 1;
       nextDay = 1;
     } else {
